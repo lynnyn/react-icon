12ef89c632425408f8c58cd019165c27f2d7dc94
Lynn Yuan <lynnyuann@gmail.com>
2020-06-29 15:45:49 +1200

    toast notification

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import './App.css';
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { IconContext } from 'react-icons'
 import {FaAngellist, FaReact} from 'react-icons/fa'
 import { MdAlarm } from 'react-icons/md'
 
+const CustomToast = ({closeToast}) =>{
+  return (
+    <div>
+      Something went wrong!
+      <button onClick = {closeToast}>CLose</button>
+    </div>
+  )
+}
+
+toast.configure()
 function App() {
+  const notify = () =>{
+    toast('basic notification',{position : toast.POSITION.TOP_LEFT})
+    toast.success('Success notification',{ 
+      position : toast.POSITION.TOP_CENTER ,
+      autoClose: 8000
+    })
+    toast.info(' info notification',{
+      position : toast.POSITION.TOP_RIGHT ,
+      autoClose: false
+    })
+    toast.warn(<CustomToast />,{position : toast.POSITION.BOTTOM_LEFT})
+    toast.error('error notification',{position : toast.POSITION.BOTTOM_CENTER})
+    toast('basic notification',{position : toast.POSITION.BOTTOM_RIGHT})
+  }
+
   return (
     <IconContext.Provider value = {{color : 'blue' , size : '5rem'}}>
     <div className="App">
         <FaReact />
-        <MdAlarm color = 'puple' size = '10rem'/>
+        <MdAlarm color = 'purple' size = '10rem'/>
+        <button onClick = {notify}>Notify</button>
     </div>
     </IconContext.Provider>
   );
